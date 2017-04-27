13ff25c193efa3db16e6ff5fcebc8cb43e7a98b2
stefaleon <stefaleon@gmail.com>
2017-04-28 00:12:10 +0300

    Add the Room class

--- Explore The House/Room.cs
+++ Explore The House/Room.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Explore_The_House
 {
-    class Room
+    class Room : Location
     {
+        private string decoration;
+
+        public Room(string name, string decoration) : base(name)
+        {
+            this.decoration = decoration;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " You see " + decoration + ".";
+            }
+        } 
+
+
     }
 }
